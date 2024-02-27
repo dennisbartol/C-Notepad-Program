3340d87cd0777b065c35510a58c3da3e62af9d8f
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2024-02-27 09:43:21 +0100

    Update npad.cpp

--- npad.cpp
+++ npad.cpp
@@ -10,3 +10,20 @@ void displayMenu() {
     cout << "2. Read from file" << endl;
     cout << "3. Exit" << endl;
 }
+
+
+void writeFile(const string& filename) {
+    ofstream file(filename);
+    if (file.is_open()) {
+        string input;
+        cout << "Enter text. Press Ctrl + Z (Windows) or Ctrl + D (Unix-like) to save and exit." << endl;
+        cin.ignore();
+        while (getline(cin, input)) {
+            file << input << endl;
+        }
+        cout << "Text saved to " << filename << endl;
+        file.close();
+    } else {
+        cerr << "Unable to open file " << filename << endl;
+    }
+}
