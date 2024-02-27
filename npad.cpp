1dea00465f3552f9eaa900ca52d2f4fe77681c2c
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2024-02-27 09:42:42 +0100

    Update npad.cpp

--- npad.cpp
+++ npad.cpp
@@ -1,3 +1,12 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+
+
+using namespace std;
+
+void displayMenu() {
+    cout << "1. Write to file" << endl;
+    cout << "2. Read from file" << endl;
+    cout << "3. Exit" << endl;
+}
